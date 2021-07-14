06c79c2daed7b463610ea6d240977b04c5957839
erripollo <pollone.enrico@gmail.com>
2021-07-14 23:53:07 +0200

    Create PageController and define home route

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'PageController@home')->name('home');
 
 Route::resource('comics', ComicController::class);
