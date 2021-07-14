c59622b658d04755313e5cb3ed57c22befbbd413
erripollo <pollone.enrico@gmail.com>
2021-07-14 17:59:21 +0200

    Create initial structure html in layout.app

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title', 'DC Comics')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+        <header>
+            Site Header
+        </header>
+
+        <main>
+            @yield('content')
+        </main>
+        
+        <footer>
+            Site Footer
+        </footer>
+    </body>
+</html>
