ede6e2ef274a5c0f22a355c2168a33ce3eb4cd9c
erripollo <pollone.enrico@gmail.com>
2021-07-15 17:34:33 +0200

    Create a edit comic button for access comics.edit

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,8 +6,12 @@
     <div class="container pt-3">
         
         <img src="{{$comic->cover_image}}" alt="">
-        <h1 class=" display-4">{{$comic->title}}</h1>
+        <div class="d-flex align-items-center">
+            <h1 class="display-4 mr-3">{{$comic->title}}</h1>
+            <a name="" id="" class="btn btn-primary" href="{{route('comics.edit', $comic->id)}}" role="button">Edit Comic</a>
+        </div>
         <p class="lead">{{$comic->description}}</p>
+
     
     </div>
 @endsection
